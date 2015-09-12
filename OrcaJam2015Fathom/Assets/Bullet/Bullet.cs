fcaee0b4c68437de00bed947be08e059cf77c54c
Nick Pettyjohn <n.pettyjohn@gmail.com>
2015-09-11 23:47:17 -0700

    Player and Bullet Movement
    Movement functions in PlayerController.cs

---
+++ OrcaJam2015Fathom/Assets/Bullet/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("bullet dir = " + transform.up.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 p = transform.position;
+		transform.position = p + transform.up * speed * Time.deltaTime;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		Destroy(gameObject);
+	}
+}
