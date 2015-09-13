5ffbec5e7e5fd7d22ad2b0dcbe6392289b412932
Mike <mike1072@gmail.com>
2015-09-12 20:20:15 -0700

    mike's stuff

---
+++ OrcaJam2015Fathom/Assets/Levels/Direction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// ballbusters: legend of the silver surfer
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West,
+}
+
+public static class Extensions
+{
+    public static Direction RotateClockwise(this Direction dir, int times)
+    {
+        return (Direction)(((int)dir + times) % 4);
+    }
+
+    public static Direction Reverse(this Direction dir)
+    {
+        return dir.RotateClockwise(2);
+    }
+}
---
+++ OrcaJam2015Fathom/Assets/Placeable/Track/Track.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// ballbusters: legend of the silver surfer
+public class Track : Placeable
+{
+    public TrackType TrackType;
+    public Direction FacingDirection;
+
+    public override bool IsTrack() { return true; }
+
+    // Use this for initialization
+    void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
